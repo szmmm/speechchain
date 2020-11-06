91ab9ec0d787b9a267a6c53933fc6dcdac8ad29b
Zhaomou Song <zs323@cam.ac.uk>
2020-11-06 12:19:48 +0000

    edit run_data_prep.sh

--- egs/babel/tts1/run.sh
+++ egs/babel/tts1/run.sh
@@ -16,10 +16,10 @@ backend=pytorch
 stage=0        # start from 0 if you need to start from data preparation
 stop_stage=100
 ngpu=1         # number of gpus ("0" uses cpu, otherwise use gpu)
+nj=32        # numebr of parallel jobs
 seed=1
 debugmode=1
 dumpdir=dump   # directory to dump full features
-N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
 verbose=0      # verbose option
 resume=""        # Resume the training from snapshot
 
--- egs/babel/tts1/run_data_prep.sh
+++ egs/babel/tts1/run_data_prep.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
 ./run.sh \
     --stage 0 \
     --stop_stage 1
