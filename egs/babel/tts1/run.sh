29ce19331f8f3d92a24e5dabc7a8779e46afa19a
Zhaomou Song <zs323@cam.ac.uk>
2020-11-07 02:12:31 +0000

    add python -m to py scripts

--- egs/babel/tts1/run.sh
+++ egs/babel/tts1/run.sh
@@ -49,7 +49,7 @@ griffin_lim_iters=1000  # the number of iterations of Griffin-Lim
 # someone else has already put it. You'll want to change this
 # if you're not on the CLSP grid.
 #datadir=/export/a15/vpanayotov/data
-
+python=python
 langs="106"
 test="106"
 
@@ -105,7 +105,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 #  done
 
   # compute global CMVN
-  compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
+  ${python} -m compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
   utils/fix_data_dir.sh data/${train_set}
 
   exp_name=$(basename $PWD)
@@ -147,7 +147,7 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
 
     echo "make a dictionary"
     echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
-    text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
+    ${python} -m text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
     | sort | uniq | grep -v -e '^\s*$' | grep -v '<unk>' | awk '{print $0 " " NR+1}' >> ${dict}
     wc -l ${dict}
 
