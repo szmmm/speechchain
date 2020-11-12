9d5f5a9c60853fdf235181633d392c269e9a8da7
Zhaomou Song <zs323@cam.ac.uk>
2020-11-12 15:57:14 +0000

    save snapshot every 2 epochs

--- espnet/asr/pytorch_backend/asr.py
+++ espnet/asr/pytorch_backend/asr.py
@@ -458,7 +458,7 @@ def train(args):
                        trigger=training.triggers.MaxValueTrigger('validation/main/acc'))
 
     # save snapshot which contains model and optimizer states
-    trainer.extend(torch_snapshot(), trigger=(1, 'epoch'))
+    trainer.extend(torch_snapshot(), trigger=(2, 'epoch'))
 
     # epsilon decay in the optimizer
     if args.opt == 'adadelta':
