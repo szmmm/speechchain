78ba3f6d8dec4f80e6bd38c0ef3d2ec2516b9f32
Zhaomou Song <zs323@cam.ac.uk>
2020-11-13 12:00:17 +0000

    test GL

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -459,7 +459,7 @@ fi
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   echo "stage 6: Synthesize............"
   #    for name in ${dev_set} ${eval_set};do
-  train_debug = GL_debug
+  train_debug=GL_debug
      for name in ${train_debug};do
         [ ! -e ${outdir}_denorm/${name} ] && mkdir -p ${outdir}_denorm/${name}
         apply-cmvn --norm-vars=true --reverse=true data/${train_set}/cmvn.ark \
