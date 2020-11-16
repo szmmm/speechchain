ef6787869b86380f3cda4ef2cd6cf3ed96620890
Zhaomou Song <zs323@cam.ac.uk>
2020-11-16 09:03:59 +0000

    check point 103

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -436,8 +436,8 @@ fi
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "stage 5: Decoding.............."
     if [ $tts_decode == 'true' ]; then
-    ttsexpdir=exp_full/tts_${tag}
-    model=snapshot.ep.80
+    ttsexpdir=exp/tts_${tag}
+    model=snapshot.ep.103
     outdir=${ttsexpdir}/outputs_${model}
     checkpoint_debug="debug_dev debug_train"
     dumpdir=dump_full
@@ -466,8 +466,8 @@ fi
 
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   echo "stage 6: Synthesize............"
-  ttsexpdir=exp_full/tts_${tag}
-  model=snapshot.ep.80
+  ttsexpdir=exp/tts_${tag}
+  model=snapshot.ep.103
   outdir=${ttsexpdir}/outputs_${model}
   checkpoint_debug="debug_dev debug_train"
   #    for name in ${dev_set} ${eval_set};do
