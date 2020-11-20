bbdc38689e7e08e9541e59987d8103c346690747
Zhaomou Song <zs323@cam.ac.uk>
2020-11-20 14:26:56 +0000

    Checking GL

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -440,7 +440,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "stage 5: Decoding.............."
     if [ $tts_decode == 'true' ]; then
     ttsexpdir=exp/tts_${tag}
-    model=snapshot.ep.150
+    model=snapshot.ep.200
     outdir=${ttsexpdir}/outputs_${model}
     checkpoint_debug="debug_dev debug_train eval_106"
 #    for name in ${dev_set} ${eval_set};do
@@ -469,9 +469,10 @@ fi
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   echo "stage 6: Synthesize............"
   ttsexpdir=exp/tts_${tag}
-  model=snapshot.ep.150
+  model=snapshot.ep.200
   outdir=${ttsexpdir}/outputs_${model}
-  checkpoint_debug="debug_dev debug_train eval_106"
+  checkpoint_debug=training_debug_GL
+#  checkpoint_debug="debug_dev debug_train eval_106"
   #    for name in ${dev_set} ${eval_set};do
      for name in ${checkpoint_debug};do
         [ ! -e ${outdir}_denorm/${name} ] && mkdir -p ${outdir}_denorm/${name}
