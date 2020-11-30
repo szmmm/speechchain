02623f7d646d3bda9bce952eab0e9c3ffb15cb18
Zhaomou Song <zs323@cam.ac.uk>
2020-11-30 20:11:14 +0000

    decode using ep174

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -453,7 +453,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "stage 5: Decoding.............."
     if [ $tts_decode == 'true' ]; then
     ttsexpdir=exp/tts_${tag}
-    model=snapshot.ep.150
+    model=snapshot.ep.174
     outdir=${ttsexpdir}/outputs_${model}
     checkpoint_debug="debug_dev debug_train"
 #    for name in ${dev_set} ${eval_set};do
@@ -482,7 +482,7 @@ fi
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   echo "stage 6: Synthesize............"
   ttsexpdir=exp/tts_${tag}
-  model=snapshot.ep.150
+  model=snapshot.ep.174
   outdir=${ttsexpdir}/outputs_${model}
 #  checkpoint_debug=training_debug_GL
   checkpoint_debug="debug_dev debug_train"
