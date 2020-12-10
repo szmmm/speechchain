722eb42098c55e600a8f4a68fa78e931dd0b9590
Zhaomou Song <zs323@cam.ac.uk>
2020-12-10 19:07:54 +0000

    resume training from ep234

--- egs/babel/asrtts/run_train_tts.sh
+++ egs/babel/asrtts/run_train_tts.sh
@@ -7,4 +7,5 @@ set -o pipefail
 
 ./run.sh \
     --stage 4 \
-    --stop_stage 4
+    --stop_stage 4 \
+    --resume /data/mifs_scratch/mjfg/zs323/yr4project/speechchain/egs/babel/asrtts/exp/tts_clean/results/snapshot.ep.234
