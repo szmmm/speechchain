901bdd4822c8cbbd25983db2e639ff3c8664ffdc
Zhaomou Song <zs323@cam.ac.uk>
2020-12-10 10:49:02 +0000

    debug 24mel using ep 100

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -450,9 +450,9 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "stage 5: Decoding.............."
     if [ $tts_decode == 'true' ]; then
     ttsexpdir=exp/tts_${tag}
-    model=snapshot.ep.304
+    model=snapshot.ep.100
     outdir=${ttsexpdir}/outputs_${model}
-    checkpoint_debug="eval_106"
+    checkpoint_debug="debug_train eval_106"
 #    for name in ${dev_set} ${eval_set};do
      for name in ${checkpoint_debug};do
         [ ! -e  ${outdir}/${name} ] && mkdir -p ${outdir}/${name}
@@ -479,10 +479,10 @@ fi
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   echo "stage 6: Synthesize............"
   ttsexpdir=exp/tts_${tag}
-  model=snapshot.ep.304
+  model=snapshot.ep.100
   outdir=${ttsexpdir}/outputs_${model}
 #  checkpoint_debug=training_debug_GL
-  checkpoint_debug="eval_106"
+  checkpoint_debug="debug_train eval_106"
   #    for name in ${dev_set} ${eval_set};do
      for name in ${checkpoint_debug};do
         [ ! -e ${outdir}_denorm/${name} ] && mkdir -p ${outdir}_denorm/${name}
