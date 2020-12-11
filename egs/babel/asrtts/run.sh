9d828156ef75037db49b9ee9a9d5a0ad8ad60c66
Zhaomou Song <zs323@cam.ac.uk>
2020-12-11 12:26:25 +0000

    decode 24mel using ep230

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -449,12 +449,10 @@ fi
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "stage 5: Decoding.............."
     if [ $tts_decode == 'true' ]; then
-    ###########ttsexpdir=exp/tts_${tag}
-    ttsexpdir=exp_40mel/tts_${tag}
-    model=snapshot.ep.304
+    ttsexpdir=exp/tts_${tag}
+    model=snapshot.ep.230
     outdir=${ttsexpdir}/outputs_${model}
-    ######checkpoint_debug="debug_train eval_106"
-    checkpoint_debug="train_sub"
+    checkpoint_debug="debug_train eval_106"
 #    for name in ${dev_set} ${eval_set};do
      for name in ${checkpoint_debug};do
         [ ! -e  ${outdir}/${name} ] && mkdir -p ${outdir}/${name}
@@ -480,12 +478,10 @@ fi
 
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   echo "stage 6: Synthesize............"
-  #####ttsexpdir=exp/tts_${tag}
-  ttsexpdir=exp_40mel/tts_${tag}
-  model=snapshot.ep.304
+  ttsexpdir=exp/tts_${tag}
+  model=snapshot.ep.230
   outdir=${ttsexpdir}/outputs_${model}
-  ####checkpoint_debug="debug_train eval_106"
-  checkpoint_debug="train_sub"
+  checkpoint_debug="debug_train eval_106"
   #    for name in ${dev_set} ${eval_set};do
      for name in ${checkpoint_debug};do
         [ ! -e ${outdir}_denorm/${name} ] && mkdir -p ${outdir}_denorm/${name}
