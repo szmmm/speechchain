7d176f9ad7decddec7e7a24f396728108ff044de
Zhaomou Song <zs323@cam.ac.uk>
2021-02-01 01:42:29 +0000

    Changing feature config for 16kHz audio

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -131,7 +131,7 @@ nnet_dir=exp/xvector_nnet_1a
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     echo "stage 0: Feature extraction for TTS and ASR"
     # for x in ${train_set} ${train_dev} ${eval_set}; do
-    for x in ${train_set} ${train_dev} ${eval_set}; do
+    for x in ${train_set}; do
         if [ ! -s data/${x}/feats.scp ]; then
         make_fbank.sh --cmd "${train_cmd}" --nj ${nj} \
             --fs ${fs} \
