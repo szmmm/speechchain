0b5d2bee00a5207213d603d6adde4fff53b22ead
Zhaomou Song <zs323@cam.ac.uk>
2021-02-03 01:55:51 +0000

    edit run_train_tts.sh

--- egs/babel/asrtts/run_train_tts.sh
+++ egs/babel/asrtts/run_train_tts.sh
@@ -7,5 +7,4 @@ set -o pipefail
 
 ./run.sh \
     --stage 4 \
-    --stop_stage 4 \
-    --resume /data/mifs_scratch/mjfg/zs323/yr4project/speechchain/egs/babel/asrtts/exp/tts_clean/results/snapshot.ep.234
+    --stop_stage 4
