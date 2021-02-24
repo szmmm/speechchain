92a65031ac48bcb28fff127d44f8ac5271e6e387
Zhaomou Song <zs323@cam.ac.uk>
2021-02-24 12:33:56 +0000

    decode english using ep166

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -410,7 +410,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "stage 5: Decoding.............."
     if [ $tts_decode == 'true' ]; then
     ttsexpdir=exp/tts_${tag}
-    model=snapshot.ep.110
+    model=snapshot.ep.166
     outdir=${ttsexpdir}/outputs_${model}
     checkpoint_debug="eval_checkpoint"
     #for name in ${eval_set};do
@@ -439,7 +439,7 @@ fi
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   echo "stage 6: Synthesize............"
   ttsexpdir=exp/tts_${tag}
-  model=snapshot.ep.110
+  model=snapshot.ep.166
   outdir=${ttsexpdir}/outputs_${model}
   #outdir=${ttsexpdir}/outputs_original_audio
   checkpoint_debug="eval_checkpoint"
