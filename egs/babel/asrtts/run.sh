a8bf9f007e3292948e865f5e3d8133ea895a2a05
Zhaomou Song <zs323@cam.ac.uk>
2021-03-11 20:31:41 +0000

    check point debug training

--- egs/babel/asrtts/run.sh
+++ egs/babel/asrtts/run.sh
@@ -413,8 +413,9 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     model=snapshot.ep.150
     outdir=${ttsexpdir}/outputs_${model}
     checkpoint_debug="eval_check"
+    checkpoint_debug2="train_check"
     #for name in ${eval_set};do
-    for name in ${checkpoint_debug};do
+    for name in ${checkpoint_debug2};do
         [ ! -e  ${outdir}/${name} ] && mkdir -p ${outdir}/${name}
         cp ${dumpdir}/${name}/data.json ${outdir}/${name}
         splitjson.py --parts ${nj} ${outdir}/${name}/data.json
@@ -443,8 +444,9 @@ if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   outdir=${ttsexpdir}/outputs_${model}
   #outdir=${ttsexpdir}/outputs_original_audio
   checkpoint_debug="eval_check"
+  checkpoint_debug2="train_check"
     #for name in ${eval_set};do
-    for name in ${checkpoint_debug};do
+    for name in ${checkpoint_debug2};do
         [ ! -e ${outdir}_denorm/${name} ] && mkdir -p ${outdir}_denorm/${name}
         apply-cmvn --norm-vars=true --reverse=true data/${train_set}/cmvn.ark \
             scp:${outdir}/${name}/feats.scp \
